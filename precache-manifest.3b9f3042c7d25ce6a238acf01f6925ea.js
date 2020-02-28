2882631efb695e791274a121bec2bf72e0e4e4bc
Mohit Gupta <AQQ@economical.com>
2020-02-28 10:29:46 -0600

    Updates

---
+++ precache-manifest.3b9f3042c7d25ce6a238acf01f6925ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1da58ea86251540ef23963a5214cbf5",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "ee1764ac6d352a6a006a",
+    "url": "/react-monsters/static/css/main.0ac1e28b.chunk.css"
+  },
+  {
+    "revision": "0d9e31d8247cb7efb3dc",
+    "url": "/react-monsters/static/js/2.6ec08681.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-monsters/static/js/2.6ec08681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1764ac6d352a6a006a",
+    "url": "/react-monsters/static/js/main.71e84206.chunk.js"
+  },
+  {
+    "revision": "26e2a3aabbe4d926badf",
+    "url": "/react-monsters/static/js/runtime-main.46fbf760.js"
+  }
+]);
